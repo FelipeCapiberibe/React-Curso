e4ad804716d87ff6acb014809f0d2f08c5c73575
Felipe Capiberibe <100447547+FelipeCapiberibe@users.noreply.github.com>
2022-10-21 00:58:03 -0300

    update

--- meuapp/src/App.js
+++ meuapp/src/App.js
@@ -1,10 +1,17 @@
- import React from 'react'
+ import React, { useState } from 'react';
+ 
  import "./App.css";
 
 const App = () => {
-
+  // let message = "Hello World";
+  const [message, setMessage] = useState('Hello World');
   return (
-    <div className='container'></div>
-  )
-}
+    <div>
+      <div className="container">{message}</div> 
+      <button onClick={() => setMessage ("Hellooo")}> 
+      Mudar mensagem
+      </button>
+    </div>
+  );
+};
 export default App;  
